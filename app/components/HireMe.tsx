207db4260daa2316e4fe9cf7d7c999f083610e37
esrasevinc <82102582+esrasevinc@users.noreply.github.com>
2024-01-06 16:49:32 +0300

    hireme button position

--- app/components/HireMe.tsx
+++ app/components/HireMe.tsx
@@ -5,7 +5,7 @@ import { DeveloperIcon } from './DeveloperIcon'
 const HireMe = () => {
 
   return (
-    <div className='absolute flex items-center md:right-8 md:top-2 md:bottom-auto dark:text-light right-8 top-10 pt-12 bottom-auto '>
+    <div className='fixed flex items-center  md:top-0 md:bottom-auto dark:text-light right-2 md:right-8 md:pt-6 lg:pt-12 top-10 bottom-auto '>
       <div className='lg:w-48 h-auto flex items-center justify-center relative w-28 md:w-30'>
        
        <DeveloperIcon 
@@ -18,8 +18,7 @@ const HireMe = () => {
           className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-900 dark:bg-light dark:text-dark text-light border-2 border-solid
           dark:border-light shadow-lg md:w-18 md:h-18 lg:w-20 lg:h-20 w-14 h-14 md:text-base lg:text-lg text-xs text-center rounded-full font-semibold hover:bg-light hover:text-dark dark:hover:bg-dark/75 dark:hover:text-light hover:border-slate-600'
           >Hire Me</Link>
-            
-            
+      
       </div>
     </div>
   )
